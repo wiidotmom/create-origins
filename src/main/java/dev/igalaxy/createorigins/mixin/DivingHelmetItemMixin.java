67ec67c9ee29863b1241d73d49eccef2954e0865
igalaxy <hey@igalaxy.dev>
2024-11-20 02:17:15 -0800

    Make diving helmet not use oxygen when in rain/under conduit power
    Closes #6

--- src/main/java/dev/igalaxy/createorigins/mixin/DivingHelmetItemMixin.java
+++ src/main/java/dev/igalaxy/createorigins/mixin/DivingHelmetItemMixin.java
@@ -4,6 +4,7 @@ import com.simibubi.create.content.equipment.armor.DivingHelmetItem;
 
 import io.github.apace100.origins.power.OriginsPowerTypes;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 
 import net.minecraft.world.level.material.Fluid;
@@ -15,10 +16,10 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(DivingHelmetItem.class)
 public class DivingHelmetItemMixin {
 	@Redirect(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z", ordinal = 0))
-	private static boolean modifyBreatheUnderwater(LivingEntity instance, TagKey<Fluid> tagKey) {
-		if (OriginsPowerTypes.WATER_BREATHING.isActive(instance)) {
-			return !instance.isEyeInFluid(tagKey);
+	private static boolean modifyBreatheUnderwater(LivingEntity entity, TagKey<Fluid> tagKey) {
+		if (OriginsPowerTypes.WATER_BREATHING.isActive(entity)) {
+			return !(entity.isEyeInFluid(tagKey) || entity.hasEffect(MobEffects.CONDUIT_POWER) || entity.hasEffect(MobEffects.WATER_BREATHING) || entity.canBreatheUnderwater() || entity.level().isRaining());
 		}
-		return instance.isEyeInFluid(tagKey);
+		return entity.isEyeInFluid(tagKey);
 	}
 }
