8c1b05b99039ae582892ff02636fdf649aa2910d
vercte <lopstedener@gmail.com>
2025-01-12 14:35:02 -0800

    allow merlings to breathe in lava with fire resistance

--- src/main/java/dev/igalaxy/createorigins/mixin/DivingHelmetItemMixin.java
+++ src/main/java/dev/igalaxy/createorigins/mixin/DivingHelmetItemMixin.java
@@ -7,20 +7,26 @@ import dev.igalaxy.createorigins.CreateOrigins;
 import io.github.apace100.origins.power.OriginsPowerTypes;
 import net.minecraft.world.entity.LivingEntity;
 
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+@Debug(export = true)
 @Mixin(DivingHelmetItem.class)
 public class DivingHelmetItemMixin {
 	@ModifyExpressionValue(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;isFireResistant()Z"))
 	private static boolean shouldNotProvideAirInLava(boolean original, LivingEntity entity) {
-		return original || CreateOrigins.merlingNeedsBacktank(entity);
+		if(OriginsPowerTypes.WATER_BREATHING.isActive(entity)) {
+			return CreateOrigins.merlingNeedsBacktank(entity);
+		}
+		return original;
 	}
 
 	@ModifyExpressionValue(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z", ordinal = 0))
 	private static boolean shouldProvideAir(boolean original, LivingEntity entity) {
 		if(OriginsPowerTypes.WATER_BREATHING.isActive(entity)) {
+			CreateOrigins.LOGGER.info("{}", CreateOrigins.merlingNeedsBacktank(entity));
 			return CreateOrigins.merlingNeedsBacktank(entity);
 		}
 		return original;
@@ -28,6 +34,12 @@ public class DivingHelmetItemMixin {
 
 	@ModifyVariable(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/equipment/armor/BacktankUtil;consumeAir(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;F)V"), ordinal = 2)
 	private static boolean modifyLavaDiving(boolean lavaDiving, LivingEntity entity) {
+		CreateOrigins.LOGGER.info("{} && !{}", lavaDiving, CreateOrigins.merlingNeedsBacktank(entity));
 		return lavaDiving && !CreateOrigins.merlingNeedsBacktank(entity);
+	}
+
+	@ModifyExpressionValue(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;noneMatch(Ljava/util/function/Predicate;)Z"))
+	private static boolean shouldCancelIfCopper(boolean original, LivingEntity entity) {
+		return original && !CreateOrigins.merlingNeedsBacktank(entity);
 	}
 }
