bb7905e545a673b2cffef54816ceb6191bc3363f
Vercte <lopstedener@gmail.com>
2025-01-12 16:34:35 -0800

    Allow Merlings to breathe in lava with netherite backtanks / fire res + backtank (#15)
    * bump ver, fix diving in lava, relocate the code for detecting if the player is a merling+needs air
    
    * allow merlings to breathe in lava with fire resistance
    
    * remove logging (oops)

--- src/main/java/dev/igalaxy/createorigins/mixin/DivingHelmetItemMixin.java
+++ src/main/java/dev/igalaxy/createorigins/mixin/DivingHelmetItemMixin.java
@@ -1,25 +1,43 @@
 package dev.igalaxy.createorigins.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.simibubi.create.content.equipment.armor.DivingHelmetItem;
 
+import dev.igalaxy.createorigins.CreateOrigins;
 import io.github.apace100.origins.power.OriginsPowerTypes;
-import net.minecraft.tags.TagKey;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 
-import net.minecraft.world.level.material.Fluid;
-
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+@Debug(export = true)
 @Mixin(DivingHelmetItem.class)
 public class DivingHelmetItemMixin {
-	@Redirect(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z", ordinal = 0))
-	private static boolean modifyBreatheUnderwater(LivingEntity entity, TagKey<Fluid> tagKey) {
-		if (OriginsPowerTypes.WATER_BREATHING.isActive(entity)) {
-			return !(entity.isEyeInFluid(tagKey) || entity.hasEffect(MobEffects.CONDUIT_POWER) || entity.level().isRainingAt(entity.blockPosition()));
+	@ModifyExpressionValue(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/Item;isFireResistant()Z"))
+	private static boolean shouldNotProvideAirInLava(boolean original, LivingEntity entity) {
+		if(OriginsPowerTypes.WATER_BREATHING.isActive(entity)) {
+			return CreateOrigins.merlingNeedsBacktank(entity);
 		}
-		return entity.isEyeInFluid(tagKey);
+		return original;
+	}
+
+	@ModifyExpressionValue(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;isEyeInFluid(Lnet/minecraft/tags/TagKey;)Z", ordinal = 0))
+	private static boolean shouldProvideAir(boolean original, LivingEntity entity) {
+		if(OriginsPowerTypes.WATER_BREATHING.isActive(entity)) {
+			return CreateOrigins.merlingNeedsBacktank(entity);
+		}
+		return original;
+	}
+
+	@ModifyVariable(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Lcom/simibubi/create/content/equipment/armor/BacktankUtil;consumeAir(Lnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/item/ItemStack;F)V"), ordinal = 2)
+	private static boolean modifyLavaDiving(boolean lavaDiving, LivingEntity entity) {
+		return lavaDiving && !CreateOrigins.merlingNeedsBacktank(entity);
+	}
+
+	@ModifyExpressionValue(method = "breatheUnderwater", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;noneMatch(Ljava/util/function/Predicate;)Z"))
+	private static boolean shouldCancelIfCopper(boolean original, LivingEntity entity) {
+		return original && !CreateOrigins.merlingNeedsBacktank(entity);
 	}
 }
